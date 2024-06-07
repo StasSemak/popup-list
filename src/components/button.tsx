7ae77270f127042e663f05e5ab47187efb53e7d0
StasSemak <semaks051@gmail.com>
2024-06-07 19:38:26 +0300

    feat: more rounded button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,7 +8,7 @@ const Button = React.forwardRef<HTMLButtonElement, Props>(
     return (
       <button
         className={cn(
-          "inline-flex items-center justify-center rounded-md text-base font-medium transition-all bg-zinc-900 hover:bg-zinc-700 text-zinc-100 h-10 py-2 px-3 border border-zinc-100",
+          "inline-flex items-center justify-center rounded-lg text-base font-medium transition-all bg-zinc-900 hover:bg-zinc-700 text-zinc-100 h-10 py-2 px-3 border border-zinc-100",
           className
         )}
         ref={ref}
