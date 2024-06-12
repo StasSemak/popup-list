cdf5cf0b0cf5edba1b08c66bda0758edfb90b650
StasSemak <semaks051@gmail.com>
2024-06-13 00:15:14 +0300

    implementation v1, 1282 error

--- src/components/webgl-widget.tsx
+++ src/components/webgl-widget.tsx
@@ -1,109 +1,49 @@
 import { useEffect, useRef } from "react"
 import { mat4 } from "gl-matrix";
+import fontData from "../assets/msdf-font.json";
+import msdfTextureSrc from "../assets/msdf-font.png";
 
-type ProgramInfo = {
-    program: WebGLProgram;
-    attribLocations: {
-        vertexPosition: number;
-        textureCoord: number;
-    };
-    uniformLocations: {
-        projectionMatrix: WebGLUniformLocation | null;
-        modelViewMatrix: WebGLUniformLocation | null;
-        uSampler: WebGLUniformLocation | null;
-    };
-}
-type Buffers = {
-    position: WebGLBuffer | null;
-    textureCoord: WebGLBuffer | null;
+type GlyphData = {
+    id: number;
+    index: number;
+    char: string;
+    width: number;
+    height: number;
+    xoffset: number;
+    yoffset: number;
+    xadvance: number;
+    chnl: number;
+    x: number;
+    y: number;
+    page: number;
 }
 
-function renderText(text: string, canvas: HTMLCanvasElement) {
-    const gl = canvas.getContext("webgl");
-    if(!gl) {
-        console.error("WebGL not supproted")
-        return;
+const vertexShaderSrc = `
+    attribute vec2 a_position;
+    attribute vec2 a_texCoord;
+    uniform mat4 u_projection;
+    varying vec2 v_texCoord;
+
+    void main() {
+      gl_Position = u_projection * vec4(a_position, 0.0, 1.0);
+      v_texCoord = a_texCoord;
+    } 
+`;
+const fragmentShaderSrc = `
+    precision mediump float;
+    uniform sampler2D u_texture;
+    varying vec2 v_texCoord;
+
+    void main() {
+      vec4 color = texture2D(u_texture, v_texCoord);
+      gl_FragColor = vec4(color.rgb, color.a);
     }
+`;
 
-    const textCanvas = document.createElement("canvas");
-    const textContext = textCanvas.getContext("2d");
-    if(!textContext) return;
-
-    textCanvas.width = 1024;
-    textCanvas.height = 512;
-    textContext.fillStyle = "black";
-    textContext.fillRect(0, 0, textCanvas.width, textCanvas.height);
-    textContext.fillStyle = "white";
-    textContext.font = "48px sans-serif";
-    textContext.fillText(text, textCanvas.width / 3, textCanvas.height / 2);
-
-    const texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, textCanvas);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    
-    const vsSource = `
-        attribute vec4 aVertexPosition;
-        attribute vec2 aTextureCoord;
-        uniform mat4 uModelViewMatrix;
-        uniform mat4 uProjectionMatrix;
-        varying highp vec2 vTextureCoord;
-        void main(void) {
-            gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-            vTextureCoord = aTextureCoord;
-        }
-    `;
-    const fsSource = `
-        varying highp vec2 vTextureCoord;
-        uniform sampler2D uSampler;
-        void main(void) {
-            gl_FragColor = texture2D(uSampler, vTextureCoord);
-        }
-    `;
-
-    const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
-    if(!shaderProgram) return;
-    const programInfo: ProgramInfo = {
-        program: shaderProgram,
-        attribLocations: {
-            vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
-            textureCoord: gl.getAttribLocation(shaderProgram, "aTextureCoord"),
-        },
-        uniformLocations: {
-            projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
-            modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
-            uSampler: gl.getUniformLocation(shaderProgram, "uSampler"),
-        }
-    };
-
-    const buffers = initBuffers(gl);
-    
-    requestAnimationFrame(() => drawScene(gl, programInfo, buffers, texture));
-}
-function initShaderProgram(gl: WebGLRenderingContext, vsSource: string, fsSource: string) {
-    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
-    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
-    if(!vertexShader || !fragmentShader) return;
-
-    const shaderProgram = gl.createProgram();
-    if(!shaderProgram) return;
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram(shaderProgram);
-
-    if(!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        console.error(`Error initializing shader program ${gl.getProgramInfoLog(shaderProgram)}`);
-        return null;
-    }
-    return shaderProgram;
-}
-function loadShader(gl: WebGLRenderingContext, type: number, source: string) {
+function createShader(gl: WebGLRenderingContext, type: number, src: string) {
     const shader = gl.createShader(type);
     if(!shader) return;
-    gl.shaderSource(shader, source);
+    gl.shaderSource(shader, src);
     gl.compileShader(shader);
 
     if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
@@ -113,80 +53,154 @@ function loadShader(gl: WebGLRenderingContext, type: number, source: string) {
     }
     return shader;
 }
-function initBuffers(gl: WebGLRenderingContext) {
-    const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    const positions = [
-        1.0, 1.0,
-        -1.0, 1.0,
-        1.0, -1.0,
-        -1.0, -1.0,
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+function createShaderProgram(gl: WebGLRenderingContext, vsSrc: string, fsSrc: string) {
+    const vertexShader = createShader(gl, gl.VERTEX_SHADER, vsSrc);
+    const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fsSrc);
+    if(!vertexShader || !fragmentShader) return;
+    
+    const shaderProgram = gl.createProgram();
+    if(!shaderProgram) return;
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
 
-    const textureCoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
-    const textureCoords = [
-        1.0, 0.0,
-        0.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0,
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
-
-    return {
-        position: positionBuffer,
-        textureCoord: textureCoordBuffer,
-    } as Buffers;
+    if(!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        console.error(`Unable to init shader program ${gl.getProgramInfoLog(shaderProgram)}`);
+        return null;
+    }
+    return shaderProgram;
 }
-function drawScene(gl: WebGLRenderingContext, programInfo: ProgramInfo, buffers: Buffers, texture: WebGLTexture | null) {
-    gl.clearColor(0, 0, 0, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+function createAndBindBuffer(gl: WebGLRenderingContext, data: number[]) {
+    const buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+    return buffer;
+}
+async function loadMsdfTexture(gl: WebGLRenderingContext): Promise<WebGLTexture | null> {
+    return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.onload = () => {
+            const texture = gl.createTexture();
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+            resolve(texture);
+        };
+        image.onerror = (err) => {
+            reject(err);
+        }
+        image.src = msdfTextureSrc;
+    })
+}
 
-    const fov = 45 * Math.PI / 180;
-    const aspect = gl.canvas.width / gl.canvas.height;
-    const zNear = 0.1;
-    const zFar = 100;
+function renderCharacter(gl: WebGLRenderingContext, glyhpData: GlyphData, x: number, 
+    y: number, positionBuffer: WebGLBuffer, texCoordBuffer: WebGLBuffer) {
+    const x0 = x + glyhpData.xoffset;
+    const x1 = x0 + glyhpData.width;
+    const y0 = y + glyhpData.yoffset;
+    const y1 = y0 + glyhpData.width;
+
+    const texX0 = glyhpData.x / fontData.common.scaleW;
+    const texX1 = (glyhpData.x + glyhpData.width) / fontData.common.scaleW;
+    const texY0 = glyhpData.y / fontData.common.scaleH;
+    const texY1 = (glyhpData.y + glyhpData.height) / fontData.common.scaleH;
+
+    const verticles = [
+        x0, y0,
+        x1, y0,
+        x0, y1,
+        x0, y1,
+        x1, y0,
+        x1, y1,
+    ];
+    const texCoords = [
+        texX0, texY0,
+        texX1, texY0,
+        texX0, texY1,
+        texX0, texY1,
+        texX1, texY0,
+        texX1, texY1,
+    ];
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verticles), gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoords), gl.STATIC_DRAW);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+}
+
+async function renderText(text: string, canvas: HTMLCanvasElement) {
+    const gl = canvas.getContext("webgl");
+    if(!gl) {
+        console.error("WebGL not supproted");
+        return;
+    }
+
+    const shaderProgram = createShaderProgram(gl, vertexShaderSrc, fragmentShaderSrc);
+    if(!shaderProgram) return;
+    gl.useProgram(shaderProgram);
+
+    let msdfTexture;
+    try {
+        msdfTexture = await loadMsdfTexture(gl);
+    } 
+    catch (err) {
+        console.error(`Error loading font texture ${err}`);
+        return;
+    }
+
+    const positionAttrLoc = gl.getAttribLocation(shaderProgram, 'a_position');
+    const texCoordAttrLoc = gl.getAttribLocation(shaderProgram, 'a_texCoord');
+    const projectionUniformLoc = gl.getUniformLocation(shaderProgram, 'u_projection');
+    const textureUniformLoc = gl.getUniformLocation(shaderProgram, 'u_texture');
+
+    const positionBuffer = createAndBindBuffer(gl, [
+        0, 0,
+        1, 0,
+        0, 1,
+        0, 1,
+        1, 0,
+        1, 1,
+    ]);
+    if(!positionBuffer) return;
+    gl.enableVertexAttribArray(positionAttrLoc);
+    gl.vertexAttribPointer(positionAttrLoc, 2, gl.FLOAT, false, 0, 0);
+
+    const texCoordBuffer = createAndBindBuffer(gl, [
+        0, 0,
+        1, 0,
+        0, 1,
+        0, 1,
+        1, 0,
+        1, 1,
+    ]);
+    if(!texCoordBuffer) return;
+    gl.enableVertexAttribArray(texCoordAttrLoc);
+    gl.vertexAttribPointer(texCoordAttrLoc, 2, gl.FLOAT, false, 0, 0);
 
     const projectionMatrix = mat4.create();
-    mat4.perspective(projectionMatrix, fov, aspect, zNear, zFar);
+    mat4.ortho(projectionMatrix, 0, canvas.width, canvas.height, 0, -1, 1);
 
-    const modelViewMatrix = mat4.create();
-    mat4.translate(modelViewMatrix, modelViewMatrix, [0.0, -0.05, -0.5]);
+    gl.uniformMatrix4fv(projectionUniformLoc, false, projectionMatrix);
 
-    {
-        const numComponents = 2;
-        const type = gl.FLOAT;
-        const normalize = false;
-        const stride = 0;
-        const offset = 0;
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-        gl.vertexAttribPointer(programInfo.attribLocations.vertexPosition, numComponents, type, normalize, stride, offset);
-        gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
-    }
-    {
-        const numComponents = 2;
-        const type = gl.FLOAT;
-        const normalize = false;
-        const stride = 0;
-        const offset = 0;
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoord);
-        gl.vertexAttribPointer(programInfo.attribLocations.textureCoord, numComponents, type, normalize, stride, offset);
-        gl.enableVertexAttribArray(programInfo.attribLocations.textureCoord);
-    }
-
-    gl.useProgram(programInfo.program);
-
-    gl.uniformMatrix4fv(programInfo.uniformLocations.projectionMatrix, false, projectionMatrix);
-    gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
-    
     gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
-    {
-        const offset = 0;
-        const vertexCount = 4;
-        gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+    gl.bindTexture(gl.TEXTURE_2D, msdfTexture);
+    gl.uniform1i(textureUniformLoc, 0);
+
+    let x = 0;
+    const y = 0;
+    for (const char of text) {
+        const glyphData = fontData.chars.find(x => x.char === char);
+
+        if(glyphData) {
+            renderCharacter(gl, glyphData, x, y, positionBuffer, texCoordBuffer);
+            x += glyphData.xadvance;
+        }
     }
 }
 
